cfabf91c38943d8075395b172e2e03da6ac12e9e
Wael Yehdego <waelwc3@gmail.com>
2020-04-30 23:28:19 -0700

    add base json format for shops

--- store/index.js
+++ store/index.js
@@ -2,7 +2,47 @@ import Vuex from 'vuex'
 
 const createStore = () => {
   return new Vuex.Store({
-    state: {},
+    state: {
+      page: 'index',
+      shops: [
+        {
+          name: 'Zen of Iroh',
+          owner: 'Uncle Iroh',
+          likes: 7034,
+          dislikes: 47,
+          menu: [
+            {
+              item: "Iroh's World Famous Tea",
+              price: 48
+            },
+            {
+              item: 'Oolong Tea',
+              price: 48
+            },
+            {
+              item: 'Jasmine Tea',
+              price: 48
+            },
+            {
+              item: 'Earl Gray Tea',
+              price: 48
+            }
+          ],
+          description: [
+            {
+              focus: ['Herbal Teas', 'Friendly Conversations'],
+              special: [
+                {
+                  name: 'Meditation',
+                  duration: [9, 11.5],
+                  days: ['Sunday', 'Thursday']
+                }
+              ]
+            }
+          ]
+        }
+      ]
+    },
     mutations: {
       updatePage(state, pageName) {
         state.page = pageName
